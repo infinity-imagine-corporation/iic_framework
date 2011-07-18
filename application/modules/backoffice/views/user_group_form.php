d32372a280809a97ac36336295f002772ccd09e5
iicmaster <iicmaster@live.com>
2011-07-18 12:58:47 +0700

    fix ajax bug

--- application/modules/backoffice/views/user_group_form.php
+++ application/modules/backoffice/views/user_group_form.php
@@ -1,5 +1,3 @@
-<form>
-	<label for="name">Name : </label>
-	<input id="name" name="name" type="text" value="<?php echo $name ?>" />
-	<input id="id" name="id" type="hidden" value="<?php echo $id ?>" />
-</form>
+<label for="name">Name : </label>
+<input id="name" name="name" type="text" value="<?php echo $name ?>" />
+<input id="id" name="id" type="hidden" value="<?php echo $id ?>" />
